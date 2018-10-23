f15d506dc5f98ce6710b419dd6ac79d162fa0cdc
benjamin-summers <summerstyler2@gmail.com>
2018-10-22 23:03:49 -0400

    created label for color

--- src/todoList.js
+++ src/todoList.js
@@ -10,10 +10,35 @@ export default class TodoList extends Component {
         }
     }
 
+    changeUserInput(input) {
+        this.setState({
+            userInput: input
+        })
+    }
+
+    addToList(input) {
+        let listArray = this.state.list;
+
+        listArray.push(input);
+
+        this.setState({
+            list: listArray
+        })
+    }
+
+
     render() {
         return (
             <div className="to-do-list-main">
-                <input type="text"/>
+                <input
+                    onChange={ (e)=>this.changeUserInput(e.target.value)}
+                    value={this.state.userInput}
+                    type="text"
+                />
+                <button onClick={() => this.addToList(this.state.userInput)}>press me</button>
+                <ul>
+                    {this.state.list.map( (val) => <li key={val.toLocaleString()}>{val}</li>)}
+                </ul>
             </div>
         )
     }
